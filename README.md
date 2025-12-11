5b662fc778c4258584c09f1dc4ae0291bf62a534
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-11 14:47:48 +0600

    chore: add package.json for zod v4 with module and type definitions

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 https://naimulcsx.notion.site/PERN-Stack-Mastery-Level-1-2bb2dc13fb6d80c78aa8d5bdb5c17c72?p=2bb2dc13fb6d80658154e7b8d17cca0a&pm=s&pvs=31 
-
+for Schema Deifne "npm install zod "
 ![alt text](image.png)
--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 
-
+const { z } = require('zod');
 const app = express();
 app.use(express.json());
 app.post('/auth/sign-up', (req, res) => {
-    console.log(req.body);
-    res.json({ message: 'Sign-up endpoint' });
+    // console.log(req.body);
+    // res.json({ message: 'Sign-up endpoint' });
     // if(req.body.firstName.lenght <= 2) {
     //     return res.status(400).json({ message: 'First name must be longer than 2 characters' });
     // }
+    const userCreateSchema = z.object({
+    firstName: z.string().min(3),
+    lastName: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(8),
+  });
+  //userCreateSchema data ta pass korietssii 
+  const output = userCreateSchema.parse(req.body);
+  console.log(output);
+
+
 });
 
 

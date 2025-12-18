f0e6383f0369b698316d4642de64a31c32e0cb77
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-18 21:53:05 +0600

    migartion done

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 Class 7
 Create two new model product variant & Product image (done)
 Talk about the relation with the product (done)
@@ -14,3 +15,23 @@ Complete the product variant controller update and delete function
 Complete the product image controller update and delete function
 Update the product controller update function
 Update the category controller update
+=======
+## Class 7 
+
+- Create two new model product variant & Product image (done)
+- Talk about the relation with the product (done)
+- Add the product image api and controller  (done)
+- Add the product variant api and controller (done)
+- Update the category model according to the doc (done)
+- Update the category controller (done);
+- Update the product model according to the doc (done)
+- Update the product controller (done)
+- Add the card and cart item model in the db (done)
+
+
+Todo 
+1. Complete the product variant controller update and delete function
+2. Complete the product image controller update and delete function
+3. Update the product controller update function
+4. Update the category controller update 
+>>>>>>> 15130b4 (migartion done)
---
+++ prisma/migrations/20251218150945_init_product_images_and_variants/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "ProductImage" (
+    "id" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "imageUrl" VARCHAR(255) NOT NULL,
+    "altText" VARCHAR(255),
+    "displayOrder" INTEGER NOT NULL DEFAULT 0,
+    "isPrimary" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductImage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductVariant" (
+    "id" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "variantName" VARCHAR(50) NOT NULL,
+    "variantValue" VARCHAR(50) NOT NULL,
+    "priceAdjustment" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
+    "stockQuantity" INTEGER NOT NULL DEFAULT 0,
+    "imageUrl" VARCHAR(255),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductVariant_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductImage" ADD CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductVariant" ADD CONSTRAINT "ProductVariant_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
